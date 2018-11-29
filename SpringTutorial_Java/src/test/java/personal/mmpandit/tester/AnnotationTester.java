cee622a6e0086337fd88d398976a00e9294012ce
mmpandit <mpandit@live.in>
2018-11-28 19:46:48 -0700

    Test STS to GitHub push

---
+++ SpringTutorial_Java/src/test/java/personal/mmpandit/tester/AnnotationTester.java
@@ -0,0 +1,19 @@
+package personal.mmpandit.tester;
+/**
+ * 
+ */
+
+/**
+ * @author mpand
+ *
+ */
+public class AnnotationTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Tester");
+	}
+
+}
