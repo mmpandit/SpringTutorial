7b6d262feafe1f605608b84aa9a395c1433df701
mmpandit <mpandit@live.in>
2018-11-28 19:54:12 -0700

    STS SSH test

---
+++ SpringTutorial_Java/src/test/java/personal/mmpandit/tester/MyTester.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package personal.mmpandit.tester;
+
+/**
+ * @author mpand
+ *
+ */
+public class MyTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("MyTester");
+
+	}
+
+}
